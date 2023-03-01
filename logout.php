7b4aeaee7107ce7f8fbcd6e8aa97908b302d6cb8
Adriano Arruda <veroticpt@gmail.com>
2023-03-01 17:06:15 -0100

    Começo do backend

---
+++ logout.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+session_destroy();
+
+header("Location: index.html");
+?>
+<?php
+
+session_start();
+
+if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] == false) {
+    header("Location: index.html");
+}
+?>
+<?php
+
+include 'verifica_login.php';
+
+echo "Bem-vindo, " . $_SESSION['username'] . ". Esta é a página inicial do administrador.";
+?>
+<?php
+?>
+
+
---
+++ verifica_login.php
@@ -0,0 +1,11 @@
+<?php
+// Iniciar sessão
+session_start();
+
+// Verificar se a sessão "logged_in" está ativa
+if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
+    // Redirecionar para a página de login
+    header("Location: login_admin.php");
+    exit;
+}
+?>
